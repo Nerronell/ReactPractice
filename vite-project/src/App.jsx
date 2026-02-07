c3057e24541609ddd76a050bc807d678925dc2e6
Nerronell <144501980+Nerronell@users.noreply.github.com>
2026-02-07 12:55:38 +0900

    Header&Footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/signin" element={<Signin />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+          </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+          <li>
+            <Link to='/'>Главная</Link>
+          </li>
+          <li>
+            <Link to='/about'>О нас</Link>
+          </li>
+          <li>
+            <Link to='/signin'>Авторизация</Link>
+          </li>
+          <li>
+            <Link to='/signup'>Регистрация</Link>
+          </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
